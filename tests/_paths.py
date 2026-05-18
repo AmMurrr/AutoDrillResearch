543679e6cd62f8fc748a89c79d4b24644efd573d
AmMurrr <gribvsevid@gmail.com>
2026-05-18 15:45:25 +0300

    update: tests better

--- tests/_paths.py
+++ tests/_paths.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 TEST_DATA_DIR = PROJECT_ROOT / "data" / "test"
-HAPPY_PERFECT_AUDIO = TEST_DATA_DIR / "happy_perfect.wav"
-HAPPY_NORMAL_AUDIO = TEST_DATA_DIR / "happy_normal.wav"
+HAPPY_PERFECT_AUDIO = TEST_DATA_DIR / "happy_perfect.mp3"
+HAPPY_NORMAL_AUDIO = TEST_DATA_DIR / "happy_normal.mp3"
 HAPPY_PROBLEM_AUDIO = TEST_DATA_DIR / "happy_problem.wav"
 HAPPY_WRONG_WORD_AUDIO = TEST_DATA_DIR / "happy_wrong_word.mp3"
 HAPPY_EMPTY_AUDIO = TEST_DATA_DIR / "happy_empty.wav"
